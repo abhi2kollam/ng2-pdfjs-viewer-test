6f8ac3b4092d25997ad56f857d9547da12ed91a1
Abhilash Pillai <abhilash.pillai@hrblock.com>
2020-11-18 22:51:05 +0530

    commits

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, ViewChild } from '@angular/core';
+import { PdfJsViewerComponent } from 'ng2-pdfjs-viewer';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ng2-pdfjs-viewer-test';
+    title = 'ng2-pdfjs-viewer-test';
+    activeItemIndex = 0;
+    public bookmarks = [];
+    @ViewChild('pdfViewer') pdfViewer: PdfJsViewerComponent;
+    constructor(private cdr: ChangeDetectorRef) {
+
+    }
+    public documentLoad(event: Event): void {
+        this.pdfViewer.PDFViewerApplication.pdfDocument
+            .getOutline()
+            .then((bookmarks) => {
+                this.bookmarks = bookmarks;
+                this.cdr.detectChanges();
+            });
+    }
+    /**
+     * goToSomePage
+     */
+    public goToSomePage(target: any, i): void {
+        this.activeItemIndex = i;
+        this.pdfViewer.PDFViewerApplication.pdfLinkService.navigateTo(target);
+    }
+    public onPageChange(event: any): void {
+        this.activeItemIndex = event - 1;
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PdfJsViewerModule } from 'ng2-pdfjs-viewer';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        PdfJsViewerModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
